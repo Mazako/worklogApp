c4db3108f6db69331ce350d749e0609b01288b64
michał <michal.maziarz12@gmail.com>
2023-10-02 19:12:07 +0200

    Basic database connection

---
+++ worklogAppBackend/src/main/java/pl/mazak/worklogappbackend/WorklogAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.mazak.worklogappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklogAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorklogAppBackendApplication.class, args);
+    }
+
+}
---
+++ worklogAppBackend/src/test/java/pl/mazak/worklogappbackend/WorklogAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mazak.worklogappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorklogAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
